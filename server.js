035289998de54d52e99c50dce326d836187262c1
Nishant Malik <nishantmalik2015@.com>
2024-02-19 00:33:21 +0530

    worked on deployemt readiness

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 const app = express();
 connectDB();
 app.use(express.json({ extended: false }));
-app.get('/', (req, res) => {
-  console.log('hellllo from API');
-});
 
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/posts', require('./routes/api/posts'));
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(__dirname, 'client', 'build', 'index.html');
+  });
+}
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log('hello world'));
