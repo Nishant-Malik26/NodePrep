600e94d4174d7983fa1a07903e7687217d9cad66
Nishant Malik <nishantmalik2015@.com>
2024-02-19 21:46:38 +0530

    fg

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,12 @@
 const express = require('express');
 const connectDB = require('./config/db');
-const cors = require('cors');
 const path = require('path');
 const app = express();
 connectDB();
 app.use(express.json({ extended: false }));
 
-// middleware
-const corsOptions = {
-  origin: 'https://frontend-snowy-six.vercel.app',
-};
+
 app.use(express.json());
-app.use(cors(corsOptions));
 
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/users', require('./routes/api/users'));
