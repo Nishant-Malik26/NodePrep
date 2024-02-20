d29e3c46a828c30509a4c2f76cc378be463a81f5
Nishant Malik <nishantmalik2015@.com>
2024-02-20 21:59:25 +0530

    clg remove

--- client/src/components/profile-forms/EditProfile.js
+++ client/src/components/profile-forms/EditProfile.js
@@ -7,11 +7,7 @@ const EditProfile = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const ProfileData = useSelector((state) => state.Profile);
-  console.log('🚀 ~ EditProfile ~ ProfileData:', ProfileData);
-  const { profile } = ProfileData.profile;
-  console.log('🚀 ~ EditProfile ~ profile:', profile);
-  const { loading } = ProfileData;
-  console.log('🚀 ~ EditProfile ~ loading:', loading);
+  const { profile, loading } = ProfileData;
   const [formData, setFormData] = useState({
     company: '',
     website: '',
