6983847692b56f4fecb3a5d6b6f9a94a4745f879
Nishant Malik <nishantmalik2015@.com>
2024-02-20 21:37:51 +0530

    gvhbj

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,7 @@ app.use((req, res, next) => {
     'Access-Control-Allow-Methods',
     'GET, POST, PUT, DELETE, OPTIONS'
   );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
 
